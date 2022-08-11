59d7e8ebdb1630c02d9e154538cfc87d7f09098a
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-12 00:12:26 +0100

    update route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import howoldController from "../controller/index.js";
+import dobValidator from "../middleware/index.js";
 
 const howoldRoute = Router();
 
-howoldRoute.get('/', howoldController);
+howoldRoute.get('/', dobValidator, howoldController);
 
 export default howoldRoute;
