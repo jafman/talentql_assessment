a47a51ad64bff82fedf38ef7ca21ce457a5b5644
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-11 23:47:39 +0100

    update configs

--- src/config.js
+++ src/config.js
@@ -1,14 +1,21 @@
 import { json, urlencoded } from 'express';
+import cors from 'cors';
+import howoldRoute from "./routes/index.js";
 
 const port = process.env.PORT || 3000;
 
 const config = (app) => {
     // adds middleware for cross-origin resource sharing configuration
     app.use(cors());
+
     // adds middleware that parses requests with x-www-form-urlencoded data encoding
     app.use(urlencoded({ extended: true }));
+
     // adds middleware that parses requests with application/json data encoding
     app.use(json());
+
+    app.use('/howold', howoldRoute);
+
     // listen
     app.listen(port, ()=>{
         console.log(`App is listening on port ${port}`);
