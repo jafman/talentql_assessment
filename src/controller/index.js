3f90dbe134122445af249c21622e471c75f54b1f
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-12 08:51:56 +0100

    update controller

--- src/controller/index.js
+++ src/controller/index.js
@@ -4,10 +4,18 @@ const howoldController = (req, res) => {
     const dob = moment(req.formatedDob).format('YYYY');
     const now = moment().format('YYYY');
     const age = now-dob;
-    res.status(200).json({
-        status: 'success',
-        age
-    })
+    if(age < 0) {
+        res.status(400).json({
+            status: 'error',
+            error: 'dob is in the future'
+        })
+    }else{
+        res.status(200).json({
+            status: 'success',
+            age
+        })
+    }
+
 }
 
 export default howoldController;
