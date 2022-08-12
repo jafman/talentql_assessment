0f9282603393803f2a6a64a2a16996564c52f420
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-12 09:18:42 +0100

    refactor middleware dir

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,24 +1,4 @@
-const dobValidator = (req, res, next) => {
-    const { query: { dob } } = req;
+import apiLimiter from "./apiLimiter.js";
+import dobValidator from "./dobValidator.js";
 
-    if(!dob) {
-        res.status(400).json({
-            status: 'error',
-            error: 'Parameter dob is missing!'
-        });
-        return;
-    }
-
-    const parsedDate = new Date(Number(dob));
-    if(isNaN(parsedDate.getTime())){
-        res.status(400).json({
-            status: 'error',
-            error: 'Invalid time stamp!'
-        });
-        return;
-    }
-
-    req.formatedDob = parsedDate;
-    next();
-}
-export default dobValidator;
+export  { apiLimiter, dobValidator };
